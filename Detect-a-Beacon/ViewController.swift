363bd2a359b63ff6e7208be6fb781ff03a234852
My Nguyen <My@mys-macbook-pro.local>
2016-08-16 17:44:55 -0700

    Requesting location: Core location

--- Detect-a-Beacon/ViewController.swift
+++ Detect-a-Beacon/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var distanceReading: UILabel!
+    var locationManager: CLLocationManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        // this corresponds to the property key NSLocationAlwaysUsageDescription set earlier
+        locationManager.requestAlwaysAuthorization()
+
+        // set the view's background color to gray
+        view.backgroundColor = UIColor.grayColor()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // this method is invoked after the user has granted or denied access to their location
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        // did the user grant access?
+        if status == .AuthorizedAlways {
+            // can the device montori iBeacons?
+            if CLLocationManager.isMonitoringAvailableForClass(CLBeaconRegion.self) {
+                // is ranging available? ranging is the abiliity to tell roughly how far something else
+                // is away from the device
+                if CLLocationManager.isRangingAvailable() {
+                    // do stuff
+                }
+            }
+        }
+    }
 }
 
